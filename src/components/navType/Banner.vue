2f07e005ef50054d58aa42c1507e2c0580e648b1
rookie <you@example.com>
2022-12-09 18:13:59 +0800

    优化走马灯

--- src/components/navType/Banner.vue
+++ src/components/navType/Banner.vue
@@ -4,7 +4,7 @@
     <!-- <el-carousel :style="{height:'600px',width:'730px'}"> -->
     <el-carousel height="550px">
       <el-carousel-item v-for="item in imgList" :key="item">
-        <img :src="item">
+        <img :src="item.url">
       </el-carousel-item>
     </el-carousel>
 </div>
@@ -16,8 +16,14 @@ export default {
     data () {
     return {
         imgList:[
-            'https://xdao.mypinata.cloud/ipfs/QmaPZHpjvZhuQHBuwAF7ms86QMQPmfCTjoG9B8tEX8ydv2',
-            'https://xdao.mypinata.cloud/ipfs/QmfA6GGBKS88sSbH8uYvDtuiipQ4wNaGmyDsmosxWCwAzB'
+            // 'https://xdao.mypinata.cloud/ipfs/QmaPZHpjvZhuQHBuwAF7ms86QMQPmfCTjoG9B8tEX8ydv2',
+            // 'https://xdao.mypinata.cloud/ipfs/QmfA6GGBKS88sSbH8uYvDtuiipQ4wNaGmyDsmosxWCwAzB'
+            {url:require('./images/lol英雄联盟至尊 魔女 莫.jpg')},
+            {url:require('./images/《卡坦岛VR Catan VR》5.jpg')},
+            {url:require('./images/《奥日与精灵意志(Ori a.jpg')},
+            {url:require('./images/新娘DVA3440x1440带鱼屏.jpg')},
+            {url:require('./images/李白 鸣剑曳影王者荣耀5.jpg')},
+            {url:require('./images/英雄联盟好运姐皮肤5120.jpg')}
           ]
         };
   },
@@ -37,12 +43,9 @@ export default {
 <style lang='less' scoped>
 .banner{
     width: 730px;
-    el-carousel-item{
-        height: 630px;
-        img{
+    img{
             width: 900px;
             height: 600px;
         }
-    }
 }
 </style>
